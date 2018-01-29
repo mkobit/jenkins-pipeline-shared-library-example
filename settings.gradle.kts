69d7735cce83d2edaf6cb11bbb6c7fd5eedfda4f
Mike Kobit <mkobit@gmail.com>
2018-01-29 12:14:45 -0600

    Convert `settings.gradle` to `settings.gradle.kts`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,10 @@ pluginManagement {
   repositories {
     gradlePluginPortal()
 //    maven {
-//      final userHome = System.getProperty('user.home')
+//      final userHome = System.getProperty("user.home")
 //      url = uri("file://${userHome}repository")
 //    }
   }
 }
 
-rootProject.name = 'jenkins-pipeline-shared-library-example'
+rootProject.name = "jenkins-pipeline-shared-library-example"
