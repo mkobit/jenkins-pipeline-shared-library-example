65967bc608b41e72b332317cba60235c172e3e68
Mike Kobit <mkobit@gmail.com>
2018-03-07 10:10:17 -0600

    ⇡ to latest plugin release and start to introduce pieces for integrating with a target Jenkins instance

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import com.gradle.scan.plugin.BuildScanPlugin
+import com.mkobit.jenkins.pipelines.http.AnonymousAuthentication
 import org.gradle.kotlin.dsl.version
 import java.io.ByteArrayOutputStream
 
 plugins {
-  id("com.gradle.build-scan") version "1.11"
-  id("com.mkobit.jenkins.pipelines.shared-library") version "0.4.0"
+  id("com.gradle.build-scan") version "1.12.1"
+  id("com.mkobit.jenkins.pipelines.shared-library") version "0.5.0"
   id("com.github.ben-manes.versions") version "0.17.0"
 }
 
@@ -27,7 +28,7 @@ buildScan {
 
 tasks {
   "wrapper"(Wrapper::class) {
-    gradleVersion = "4.5.1"
+    gradleVersion = "4.6"
   }
 }
 
@@ -36,13 +37,22 @@ java {
 }
 
 dependencies {
-  testImplementation("org.spockframework:spock-core:1.1-groovy-2.4")
+  val spock = "org.spockframework:spock-core:1.1-groovy-2.4"
+  testImplementation(spock)
   testImplementation("org.assertj:assertj-core:3.9.1")
+  integrationTestImplementation(spock)
+}
+
+jenkinsIntegration {
+  baseUrl.set(uri("http://localhost:5050").toURL())
+  authentication.set(providers.provider { AnonymousAuthentication })
+  downloadDirectory.set(layout.projectDirectory.dir("jenkinsResources"))
 }
 
 sharedLibrary {
-  coreVersion = "2.95"
-  groovyVersion = "2.4.12"
+  // TODO: this will need to be altered when auto-mapping functionality is complete
+  coreVersion = jenkinsIntegration.downloadDirectory.file("core-version.txt").get().asFile.readText()
+  // TODO: retrieve downloaded plugin resource
   pluginDependencies(Action {
     dependency("org.jenkinsci.plugins", "pipeline-model-api", "1.2.5")
     dependency("org.jenkinsci.plugins", "pipeline-model-declarative-agent", "1.1.1")
